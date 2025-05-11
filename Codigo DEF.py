6d2cecc36080f7d4cd996b7942046f4c8257b6ae
sarahmonteiroanjos <sarah.monteiro@sempreceub.com>
2025-05-10 23:50:12 -0300

    Add files via upload

---
+++ Codigo DEF.py
@@ -0,0 +1,11 @@
+def mostrar_mensagem_e_numero(mensagem, numero):
+    print("Mensagem:", mensagem)
+    print("Número:", numero)
+if __name__ == "__main__":
+    sms= input("Digite a mensagem: ")
+    num= int(input("Digite o número: "))
+    sms_num_digitado = mostrar_mensagem_e_numero(sms,num)
+
+
+
+
